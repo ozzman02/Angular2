dbf63beafed9160b7273ac3fe8d1592157ab8ee6
Oscar Santamaria <osantamaria@gmail.com>
2020-03-13 00:36:59 -0600

    agregando anotacion CrossOrigin

--- restaurantes/back-end/restaurantes-api/src/main/java/com/restaurantes/controller/RestauranteController.java
+++ restaurantes/back-end/restaurantes-api/src/main/java/com/restaurantes/controller/RestauranteController.java
@@ -3,6 +3,7 @@ package com.restaurantes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.restaurantes.service.RestauranteService;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class RestauranteController {
 
 	private RestauranteService restauranteService;
