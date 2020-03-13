61e0d4382294077b79fdc435e299df52b8230c2d
Oscar Santamaria <osantamaria@gmail.com>
2020-03-12 23:26:36 -0600

    Creando controllador y configuracion basica de swagger

--- restaurantes/back-end/restaurantes-api/src/main/java/com/restaurantes/controller/RestauranteController.java
+++ restaurantes/back-end/restaurantes-api/src/main/java/com/restaurantes/controller/RestauranteController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurantes.service.RestauranteService;
 
 @RestController
+@RequestMapping("/api/v1")
 public class RestauranteController {
 
 	private RestauranteService restauranteService;
